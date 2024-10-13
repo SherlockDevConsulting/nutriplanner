a7e4a80c92d68ad0adbaf26ba231b96e950a5d2e
Sherlock <martinpdev@protonmail.com>
2024-10-13 19:27:02 +0200

    🦄 refactor(formatter): formatting files

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,20 +4,21 @@ from app.config.config import Config
 from app.config.logging_config import setup_logging
 
 
-
 db = SQLAlchemy()
 
 
 def create_app():
+    """Setup the flask application"""
     app = Flask(__name__)
 
-    #Init Configuration Object in app
+    # Init Configuration Object in app
     app.config.from_object(Config)
 
     db.init_app(app)
 
     # pylint: disable=C0415
     from app.routes import register_routes
+
     register_routes(app)
 
     setup_logging()
